c65d809e041b37c6e3e2ebc884a5291f33a2007a
viacompiyush <viacom.piyush@gmail.com>
2014-12-16 20:01:45 +0530

    Update dfp_728x90.js

--- src/BTG/iOS/AdConfig/dfp/dfp_728x90.js
+++ src/BTG/iOS/AdConfig/dfp/dfp_728x90.js
@@ -2,7 +2,7 @@
 	{
 		"config" : {
 			"isCoppaCompliant" : true,
-                         "audienceManagerEnabled": false
+                         "audienceManagerEnabled": true
 
 		},
 		"adData" : {
