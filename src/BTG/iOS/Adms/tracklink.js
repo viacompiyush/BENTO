03d96e25c4e544a0010f376cfdf43484a89161c8
viacompiyush <viacom.piyush@gmail.com>
2014-12-16 19:56:18 +0530

    Update tracklink.js

--- src/BTG/iOS/Adms/tracklink.js
+++ src/BTG/iOS/Adms/tracklink.js
@@ -2,7 +2,7 @@
     {
         "linkName" : "dev/bento/remote/link",
         "prop1"    : "remote value of prop 1",
-        "pageName" : "cod",
+        "pageName" : "dev/bento/remote/testrig",
         "prop11"    : "remote value of prop 11 tracklink",
         "prop74"    : "remote value of prop 74 tracklink",
         "prop75"    : "remote value of prop 75 tracklink",
