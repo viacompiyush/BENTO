666665a5540cc952b1dc24e05bf0d2312ac3f09a
viacompiyush <viacom.piyush@gmail.com>
2014-12-17 16:49:18 +0530

    Update trackpage.js

--- src/BTG/iOS/Adms/trackpage.js
+++ src/BTG/iOS/Adms/trackpage.js
@@ -14,6 +14,7 @@
         "eVar74" : "remote value of evar 74 track page",
         "eVar111" : "remote value of evar 111 track page",
         "eVar300" : "remote value of evar 300 track page",
-        "channel" : "remote sandbox channel track page"
+        "channel" : "remote sandbox channel track page",
+        "VisitorID" : "",
 }
 ]
