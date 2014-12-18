7b4520b991eaaeec88c49eff49c792dc493a63a2
viacompiyush <viacom.piyush@gmail.com>
2014-12-18 18:20:10 +0530

    Create dfp_config.js

---
+++ src/BTG/Android/AdConfig/dfp/dfp_config.js
@@ -0,0 +1,25 @@
+[{
+	"omnitureEnabled": "true",
+	"omnitureSuite": "viarnd",
+	"omnitureChannel": "Test Rig Channel",
+	"omnitureCharSet": "ISO-8859-1",
+	"omnitureCurrencyCode": "USD",
+	"omnitureSSL": "false",
+	"omnitureLifecycleSessionTimeout": "300",
+	"omnitureTrackingServer": "sc.mtv.com",
+	"omnitureVisitorID": "",
+	"omnitureDebugLogging": "true",
+	"omnitureOfflineTrackingEnabled": "true",
+	"omnitureOfflineHitLimit": 1000,
+	"demdexEnabled": "true",
+	"TVEEnabled": "true",
+	"omnitureContextData": {
+		"v.dummyContext1": "1",
+		"v.dummyContext2": "2"
+	},
+	"omnitureTimetrackingEnabled": "true",
+	"doubleclickEnabled": "true",
+	"doubleClickStaticAdAppId": "android_test_app",
+	"doubleclickUseExternalBrowser": true,
+	"doubleClickStaticAdIsCoppaCompliant": true
+}]
