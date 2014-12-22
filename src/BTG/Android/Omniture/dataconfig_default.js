5ed072433fb24fcb0cd974a9d0206aa977951d45
viacompiyush <viacom.piyush@gmail.com>
2014-12-22 16:05:22 +0530

    Update dataconfig_default.js

--- src/BTG/Android/Omniture/dataconfig_default.js
+++ src/BTG/Android/Omniture/dataconfig_default.js
@@ -1,6 +1,6 @@
 [
     {
-        "pageName" : "dev/bento/link",
+        "pageName" : "blah!!",
         "linkName" : "dev/bento/link",
         "prop1"    : "value of prop 1",
         "eVar1"    : "value of evar 1",
