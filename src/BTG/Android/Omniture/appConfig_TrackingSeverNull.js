b0542e041dd83c6a3b0c46af6d7ce3203bbf44e9
viacompiyush <viacom.piyush@gmail.com>
2014-12-22 15:58:31 +0530

    Update appConfig_TrackingSeverNull.js

--- src/BTG/Android/Omniture/appConfig_TrackingSeverNull.js
+++ src/BTG/Android/Omniture/appConfig_TrackingSeverNull.js
@@ -6,7 +6,7 @@
 		"omnitureChannel" : "TestChannel",
 		"omnitureCharSet" : "ISO-8859-1",
 		"omnitureCurrencyCode" : "USD",
-		"omnitureSSL" : "",
+		"omnitureSSL" : "NULL",
 		"omnitureLifecycleSessionTimeout" : "300",
 		"omnitureTrackingServer" : "",
 		"omnitureVisitorID" : "2",
