7e881d7d305735bd75be54a0b4dc8876fe320670
viacompiyush <viacom.piyush@gmail.com>
2014-12-23 20:18:44 +0530

    Update dfp_config.js

--- src/BTG/Android/AdConfig/dfp/dfp_config.js
+++ src/BTG/Android/AdConfig/dfp/dfp_config.js
@@ -21,5 +21,5 @@
 	"doubleclickEnabled": "true",
 	"doubleClickStaticAdAppId": "android_test_app",
 	"doubleclickUseExternalBrowser": "false",
-	"doubleClickStaticAdIsCoppaCompliant": false
+	"doubleClickStaticAdIsCoppaCompliant": "false"
 }]
