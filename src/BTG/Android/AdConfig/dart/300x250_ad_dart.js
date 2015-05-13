a162162365dffafbcec0dd62a6fb2d52f17d7523
viacompiyush <viacom.piyush@gmail.com>
2015-05-13 13:18:00 +0530

    Update 300x250_ad_dart.js

--- src/BTG/Android/AdConfig/dart/300x250_ad_dart.js
+++ src/BTG/Android/AdConfig/dart/300x250_ad_dart.js
@@ -1,6 +1,6 @@
 [
     {
-	"doubleclickKeyValues":"celtra_mobile_300x250",
+	"doubleclickKeyValues":"testmode=celtra_mobile_300x250",
 	"doubleclickAdSize":"300x250",
 	"doubleclickIsDfp":false,
 	"doubleclickSlotNumber":2,
