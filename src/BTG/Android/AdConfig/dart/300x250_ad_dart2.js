fc2e2c74e39528813b49b69b95637629907196bf
viacompiyush <viacom.piyush@gmail.com>
2015-05-14 08:11:58 -0400

    Create 300x250_ad_dart2.js

---
+++ src/BTG/Android/AdConfig/dart/300x250_ad_dart2.js
@@ -0,0 +1,30 @@
+[
+{
+"doubleclickKeyValues":"testmode=medialets_mobile_300x50",
+"doubleclickAdSize":"300x50",
+"doubleclickSlotNumber":2,
+"doubleclickDfpData":
+{
+"metadata": {
+"property": "mtv",
+"hub": "shows",
+"franchise": "Teen Mom 2",
+"section": "full-episodes",
+"title": "A Teen Mom Gets Pregnant.",
+"is_main": true,
+"season": 2,
+"episode": "202"
+},
+"mappings": {
+"ad-unit": {
+"1": "@property"
+},
+"key-values": {
+"sea": "@season",
+"ep": "@episode",
+"foo": "bar"
+}
+}
+}
+}
+]
